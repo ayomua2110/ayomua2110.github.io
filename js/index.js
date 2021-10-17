ffedf9d9f66a9bd15335f5f184bda9099299646c
ayomua2110 <90565897+ayomua2110@users.noreply.github.com>
2021-10-17 20:13:22 +0800

    123

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||仕|華|舅|媽|生|日|快|樂|#rectangle||祝||生|日|快|樂|#rectangle|');
     }
 
     S.Drawing.loop(function () {
